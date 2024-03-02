b46fe347196f70c0297d83b2fa042df8dd57b07e
Cedric Glou <cedricglou@MacBook-Air-de-Cedric.local>
2024-03-02 18:53:21 +0100

    v1 fini debut de la v2 factorisation du code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 "use strict";
-// 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,22 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-// 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
-// 3 - Passer les données à une variable
-// 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
-// 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-// coutry.name.includes(inputSearch.value);
-// 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
-// 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
+const countriesBox = document.querySelector('.countries-container');
+// toLocaleString() = séparateur de millier
+// Le tableau ou seront stocké les données
 let arrayCountries = [];
 const inputRange = document.querySelector('#inputRange');
-console.log(inputRange.value);
 const rangeValue = document.querySelector('#rangeValue');
-let counterCountry;
 inputRange.addEventListener('input', (e) => {
     rangeValue.innerHTML = e.target.value;
     countriesBox.innerHTML = "";
     console.log(inputRange.value);
+    // à chaque fois que la valuer change tu rappel la fonction createCart 
     createCart();
 });
 const fetchData = () => __awaiter(void 0, void 0, void 0, function* () {
@@ -37,7 +31,7 @@ const createCart = () => __awaiter(void 0, void 0, void 0, function* () {
     yield fetchData();
     console.log(arrayCountries);
     let range = inputRange.value;
-    let newArr = arrayCountries.slice(-range);
+    let newArr = arrayCountries.slice(0, range);
     console.log(newArr);
     countriesBox.innerHTML = "";
     return newArr
@@ -48,37 +42,44 @@ const createCart = () => __awaiter(void 0, void 0, void 0, function* () {
        <h3>${country.name.common}</h3>
        <h4>${country.capital ? country.capital : ""}</h4>
        <p>Population: ${country.population.toLocaleString()}</p>
-       
        </div>`;
     });
 });
-createCart();
+// au chargement tu appelles la fonction createCart 
+window.addEventListener('load', createCart);
+// --------------
+// Ordre Alphabetique  
+// --------------
 const alphabet = () => __awaiter(void 0, void 0, void 0, function* () {
     yield fetchData();
+    let range = inputRange.value;
+    let newArr = arrayCountries.slice(0, range);
     countriesBox.innerHTML = "";
-    arrayCountries
+    newArr
         .sort((a, b) => { return a.name.common.localeCompare(b.name.common); }).map((country) => {
         countriesBox.innerHTML += `
-       
-       <div class="card-container">
+        <div class="card-container">
             <img class="flag" src=${country.flags.svg} alt=${country.flags.alt} />
              <h3>${country.name.common}</h3>
              <h4>${country.capital ? country.capital : ""}</h4>
-             <p>Population: ${country.population.toLocaleString()}</p>
-              
-              
-             </div>
+             <p>Population: ${country.population.toLocaleString()}</p>  
+        </div>
        
         `;
     });
 });
 const alphabetic = document.querySelector("#alpha");
 alphabetic.addEventListener('click', alphabet);
+// --------------
+// Croissant 
+// --------------
 const croissant = document.querySelector("#minToMax");
 const minToMax = () => __awaiter(void 0, void 0, void 0, function* () {
     yield fetchData();
+    let range = inputRange.value;
+    let newArr = arrayCountries.slice(0, range);
     countriesBox.innerHTML = "";
-    arrayCountries
+    newArr
         .sort((a, b) => { return a.population - b.population; }).map((country) => {
         countriesBox.innerHTML += `
        
@@ -95,43 +96,34 @@ const minToMax = () => __awaiter(void 0, void 0, void 0, function* () {
     });
 });
 croissant.addEventListener('click', minToMax);
+// --------------
+// Décroissant 
+// --------------
 const decroissant = document.querySelector("#maxToMin");
 const maxToMin = () => __awaiter(void 0, void 0, void 0, function* () {
     yield fetchData();
+    let range = inputRange.value;
+    let newArr = arrayCountries.slice(0, range);
     countriesBox.innerHTML = "";
-    arrayCountries
+    newArr
         .sort((a, b) => { return b.population - a.population; }).map((country) => {
         countriesBox.innerHTML += `
        
-       <div class="card-container">
+        <div class="card-container">
             <img class="flag" src=${country.flags.svg} alt=${country.flags.alt} />
-             <h3>${country.name.common}</h3>
-             <h4>${country.capital ? country.capital : ""}</h4>
-             <p>Population: ${country.population.toLocaleString()}</p>
-              
-              
-             </div>
+            <h3>${country.name.common}</h3>
+            <h4>${country.capital ? country.capital : ""}</h4>
+            <p>Population: ${country.population.toLocaleString()}</p>
+        </div>
        
         `;
     });
 });
 decroissant.addEventListener('click', maxToMin);
-//  let arrayC = ['SOMALIA','KENYA','JERSEY','CHAD','DOMINICA','BENIN','AUSTRALIA']
-//  arrayC.sort((a:any,b:any) =>(b-a));
-//  console.log(arrayC);
-// var people = [
-//     { name: "Charlie", age: 28 },
-//     { name: "Alice", age: 25 },
-//     { name: "Bob", age: 30 },
-//   ];
-// people.sort(function(a:any,b:any) {return a.name.localeCompare(b.name)})
-// console.log(people);
-//  croissant.addEventListener('click', alphabet)
-const countriesBox = document.querySelector('.countries-container');
 const inputSearch = document.querySelector("#inputSearch");
 inputSearch.addEventListener('input', () => {
+    // il faut mettre toutes les recherches et les pays recherchés en miniscule 
     let word = inputSearch.value.toLowerCase();
-    console.log(word);
     let newArray = arrayCountries.filter((country) => {
         let lower = country.name.common.toLowerCase();
         return lower.includes(word);
@@ -141,14 +133,11 @@ inputSearch.addEventListener('input', () => {
         countriesBox.innerHTML += `
        
         <div class="card-container">
-             <img class="flag" src=${country.flags.svg} alt=${country.flags.alt} />
-              <h3>${country.name.common}</h3>
-              <h4>${country.capital ? country.capital : ""}</h4>
-              <p>Population: ${country.population.toLocaleString()}</p>
-               
-               
-              </div>
-        
+            <img class="flag" src=${country.flags.svg} alt=${country.flags.alt} />
+            <h3>${country.name.common}</h3>
+            <h4>${country.capital ? country.capital : ""}</h4>
+            <p>Population: ${country.population.toLocaleString()}</p>
+        </div>
          `;
     });
 });
--- index.ts
+++ index.ts
@@ -1,33 +1,17 @@
-// 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
+const countriesBox:any = document.querySelector('.countries-container')
+// toLocaleString() = séparateur de millier
 
-// 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
-
-// 3 - Passer les données à une variable
-
-// 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
-
-// 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-// coutry.name.includes(inputSearch.value);
-
-// 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
-
-// 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
-
-let arrayCountries:any = []
-
+// Le tableau ou seront stocké les données
+let arrayCountries:any = [];
 const inputRange:any = document.querySelector('#inputRange');
-console.log(inputRange.value)
 const rangeValue:any = document.querySelector('#rangeValue');
-
-let counterCountry:any; 
 
 inputRange.addEventListener('input',(e:any)=>{
     rangeValue.innerHTML = e.target.value;
     countriesBox.innerHTML ="";
     console.log(inputRange.value)
-
+    // à chaque fois que la valuer change tu rappel la fonction createCart 
    createCart();
-     
 })
 
 const fetchData = async ()=>{
@@ -41,7 +25,7 @@ const createCart = async()=>{
    await fetchData();
    console.log(arrayCountries);
    let range:number = inputRange.value;
-  let newArr = arrayCountries.slice(-range);
+  let newArr = arrayCountries.slice(0, range);
 
    console.log(newArr);
    countriesBox.innerHTML ="";
@@ -54,48 +38,52 @@ const createCart = async()=>{
        <h3>${country.name.common}</h3>
        <h4>${country.capital ? country.capital : ""}</h4>
        <p>Population: ${country.population.toLocaleString()}</p>
-       
        </div>`
    })
-   
 }
-createCart();
+// au chargement tu appelles la fonction createCart 
+window.addEventListener('load',createCart)
 
+// --------------
+// Ordre Alphabetique  
+// --------------
 const alphabet = async()=>{
     await fetchData();
+    let range:number = inputRange.value;
+    let newArr = arrayCountries.slice(0, range);
+    
     countriesBox.innerHTML ="";
-     arrayCountries
+    newArr
      .sort((a:any,b:any)=> {return a.name.common.localeCompare(b.name.common)}).map((country:any)=>{
-            
-
        countriesBox.innerHTML += `
-       
-       <div class="card-container">
+        <div class="card-container">
             <img class="flag" src=${country.flags.svg} alt=${country.flags.alt} />
              <h3>${country.name.common}</h3>
              <h4>${country.capital ? country.capital : ""}</h4>
-             <p>Population: ${country.population.toLocaleString()}</p>
-              
-              
-             </div>
+             <p>Population: ${country.population.toLocaleString()}</p>  
+        </div>
        
         `
     })
     
  }
 
- const alphabetic:any = document.querySelector("#alpha");
+const alphabetic:any = document.querySelector("#alpha");
 
- alphabetic.addEventListener('click', alphabet)
+alphabetic.addEventListener('click', alphabet)
 
-
-
-
+// --------------
+// Croissant 
+// --------------
 const croissant:any = document.querySelector("#minToMax");
 const minToMax = async()=>{
     await fetchData();
+
+    let range:number = inputRange.value;
+    let newArr = arrayCountries.slice(0, range);
+
     countriesBox.innerHTML ="";
-     arrayCountries
+     newArr
      .sort((a:any,b:any)=> {return a.population - b.population}).map((country:any)=>{
             
 
@@ -115,61 +103,46 @@ const minToMax = async()=>{
     
  }
  croissant.addEventListener('click', minToMax)
-
+// --------------
+// Décroissant 
+// --------------
 const decroissant:any = document.querySelector("#maxToMin");
 const maxToMin = async()=>{
     await fetchData();
+    let range:number = inputRange.value;
+    let newArr = arrayCountries.slice(0, range);
     countriesBox.innerHTML ="";
-     arrayCountries
+    newArr
      .sort((a:any,b:any)=> {return  b.population - a.population}).map((country:any)=>{
             
-
        countriesBox.innerHTML += `
        
-       <div class="card-container">
+        <div class="card-container">
             <img class="flag" src=${country.flags.svg} alt=${country.flags.alt} />
-             <h3>${country.name.common}</h3>
-             <h4>${country.capital ? country.capital : "" }</h4>
-             <p>Population: ${country.population.toLocaleString()}</p>
-              
-              
-             </div>
+            <h3>${country.name.common}</h3>
+            <h4>${country.capital ? country.capital : "" }</h4>
+            <p>Population: ${country.population.toLocaleString()}</p>
+        </div>
        
         `
     })
     
  }
- decroissant.addEventListener('click', maxToMin)
-//  let arrayC = ['SOMALIA','KENYA','JERSEY','CHAD','DOMINICA','BENIN','AUSTRALIA']
-//  arrayC.sort((a:any,b:any) =>(b-a));
-//  console.log(arrayC);
 
-// var people = [
-//     { name: "Charlie", age: 28 },
-//     { name: "Alice", age: 25 },
-//     { name: "Bob", age: 30 },
-//   ];
- 
-// people.sort(function(a:any,b:any) {return a.name.localeCompare(b.name)})
- 
-// console.log(people);
-//  croissant.addEventListener('click', alphabet)
+decroissant.addEventListener('click', maxToMin)
 
 
 
 
-
-
-
-
-const countriesBox:any = document.querySelector('.countries-container')
 
 const inputSearch:any = document.querySelector("#inputSearch");
 
+
 inputSearch.addEventListener('input', ()=>{
    
+    // il faut mettre toutes les recherches et les pays recherchés en miniscule 
     let word = inputSearch.value.toLowerCase();
-    console.log(word);
+    
     
     let newArray:any = arrayCountries.filter((country:any ) =>{
     let lower:string = country.name.common.toLowerCase();
@@ -183,16 +156,11 @@ inputSearch.addEventListener('input', ()=>{
         countriesBox.innerHTML += `
        
         <div class="card-container">
-             <img class="flag" src=${country.flags.svg} alt=${country.flags.alt} />
-              <h3>${country.name.common}</h3>
-              <h4>${country.capital ? country.capital : "" }</h4>
-              <p>Population: ${country.population.toLocaleString()}</p>
-               
-               
-              </div>
-        
+            <img class="flag" src=${country.flags.svg} alt=${country.flags.alt} />
+            <h3>${country.name.common}</h3>
+            <h4>${country.capital ? country.capital : "" }</h4>
+            <p>Population: ${country.population.toLocaleString()}</p>
+        </div>
          `
     })
-   
-
 })
